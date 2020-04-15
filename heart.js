de603b2de2dbe5c453ae09fce1042e371abca7fd
Yuichi Tanimoto <>
2020-04-15 16:37:05 +0900

    update heart

--- heart.js
+++ heart.js
@@ -1,28 +1,23 @@
-function heartWithColor(x, y, color) {
-  beginShape();
-  vertex(x, y);
-  fill(color[0], color[1], color[2]);
-  bezierVertex(x + 30, y - 30, x + 70, y + 20, x, y + 60);
-  bezierVertex(x - 70, y + 20, x - 30, y - 30, x, y);
-  endShape();
-}
-
-function heartWithSize(x, y, size) {
+function drawHeartTop(x, y, size) {
   beginShape();
   vertex(x, y);
   bezierVertex(x + size / 2, y - size / 2,
                x + size * 7 / 6, y + size / 3,
                x, y + size);
-  bezierVertex(x - size * 7 / 6, y + size / 3,
+  bezierVertex(x - size * 7 / 6, y + size / 3, 
                x - size / 2, y - size / 2,
                x, y);
   endShape();
 }
 
-function heart(x, y) {
+function drawHeartCenter(x, y, size) {
   beginShape();
-  vertex(x, y);
-  bezierVertex(x + 30, y - 30, x + 70, y + 20, x, y + 60);
-  bezierVertex(x - 70, y + 20, x - 30, y - 30, x, y);
+  vertex(x, y - size / 2);
+  bezierVertex(x + size / 2, y - size,
+               x + size * 7 / 6, y - size / 6,
+               x, y + size / 2);
+  bezierVertex(x - size * 7 / 6, y - size / 6,
+               x - size / 2, y - size,
+               x, y - size / 2);
   endShape();
 }
